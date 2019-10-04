7b1a538516481c84105865c7a1d10ad1b120be77
Stephanie Davidson <rpookie98@gmail.com>
2019-10-04 18:09:24 -0400

    started over with bullet code lol

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, logging, json, arcade
+import sys, logging, os, random, math, arcade
 
 #check to make sure we are running the right version of Python
 version = (3,7)
@@ -10,87 +10,116 @@ logger = logging.getLogger(__name__)
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Space Shooter"
-MOVEMENT_SPEED = 5
+MARGIN = 30
+SCREEN_TITLE = "Bullet exercise"
+
+NUM_ENEMIES = 5
+STARTING_LOCATION = (400,100)
+BULLET_DAMAGE = 10
+ENEMY_HP = 100
+HIT_SCORE = 10
+KILL_SCORE = 100
+
+class Bullet(arcade.Sprite):
+    def __init__(self, position, velocity, damage):
+        ''' 
+        initializes the bullet
+        Parameters: position: (x,y) tuple
+            velocity: (dx, dy) tuple
+            damage: int (or float)
+        '''
+        super().__init__("assets/bullet.png", 0.5)
+        (self.center_x, self.center_y) = position
+        (self.dx, self.dy) = velocity
+        self.damage = damage
+
+    def update(self):
+        '''
+        Moves the bullet
+        '''
+        self.center_x += self.dx
+        self.center_y += self.dy
+
+
+    
+class Player(arcade.Sprite):
+    def __init__(self):
+        super().__init__("assets/PNG/playerShip2_blue.png", 0.5)
+        (self.center_x, self.center_y) = STARTING_LOCATION
+
+class Enemy(arcade.Sprite):
+    def __init__(self, position):
+        '''
+        initializes a penguin enemy
+        Parameter: position: (x,y) tuple
+        '''
+        super().__init__("assets/PNG/shipBeige_manned.png", 0.5)
+        self.hp = ENEMY_HP
+        (self.center_x, self.center_y) = position
+
+
+        
 
 
 class Window(arcade.Window):
 
     def __init__(self, width, height, title):
-
-        # Call the parent class's init function
         super().__init__(width, height, title)
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
 
-        # Make the mouse disappear when it is over the window.
-        # So we just see our object, not the pointer.
-        self.set_mouse_visible(False)
-
+        self.set_mouse_visible(True)
         arcade.set_background_color(arcade.color.BLACK_LEATHER_JACKET)
-
-        # Sprite lists
-        self.player_list = arcade.SpriteList()
-
-        # Set up the player
+        self.bullet_list = arcade.SpriteList()
+        self.enemy_list = arcade.SpriteList()
+        self.player = Player()
         self.score = 0
-
-
-
 
     def setup(self):
-
-
-        # Set up the player
-        self.score = 0
-        self.player = arcade.Sprite('assets/PNG/playerShip2_blue.png', 1)
-        self.player.center_x = SCREEN_WIDTH // 2
-        self.player.center_y = SCREEN_HEIGHT // 2
-        self.player_list.append(self.player)
-
-
+        '''
+        Set up enemies
+        '''
+        for i in range(NUM_ENEMIES):
+            x = 120 * (i+1) + 40
+            y = 500
+            enemy = Enemy((x,y))
+            self.enemy_list.append(enemy)            
 
     def update(self, delta_time):
-        pass
+        self.bullet_list.update()
+        for e in self.enemy_list:
+            for b in self.bullet_list:
+                collisions = e.collides_with_list(self.bullet_list)
+                for c in collisions:
+                    e.hp -= BULLET_DAMAGE
+                    self.bullet_list.remove(b)
+                    if e.hp <= 0:
+                        self.enemy_list.remove(e)
+                        self.score += KILL_SCORE
+
+            
 
     def on_draw(self):
-        """ Called whenever we need to draw the window. """
         arcade.start_render()
-        self.player_list.draw()
-
-
-
+        arcade.draw_text(str(self.score), 20, SCREEN_HEIGHT - 40, arcade.color.GHOST_WHITE, 16)
+        self.player.draw()
+        self.bullet_list.draw()
+        self.enemy_list.draw()
+        if len(self.enemy_list) == 0:
+            arcade.draw_text('YOU WIN', 375, SCREEN_HEIGHT - 40, arcade.color.GHOST_WHITE, 24)
 
     def on_mouse_motion(self, x, y, dx, dy):
-        """ Called to update our objects. Happens approximately 60 times per second."""
-        pass
+        '''
+        The player moves left and right with the mouse
+        '''
+        self.player.center_x = x
 
     def on_mouse_press(self, x, y, button, modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
-
-    def on_mouse_release(self, x, y, button, modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
-
-    def on_key_press(self, key, modifiers):
-        """ Called whenever the user presses a key. """
-        if key == arcade.key.LEFT:
-            self.player.center_y += MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT:
-            self.player.center_y -= MOVEMENT_SPEED
-        elif key == arcade.key.UP:
-            self.player.center_x -= MOVEMENT_SPEED
-        elif key == arcade.key.DOWN:
-            self.player.center_x += MOVEMENT_SPEED
-
-    def on_key_release(self, key, modifiers):
-        """ Called whenever a user releases a key. """
-        #When the user releases the keys, the players movement stops.
-        pass
-
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            x = self.player.center_x
+            y = self.player.center_y + 15
+            bullet = Bullet((x,y),(0,10),BULLET_DAMAGE)
+            self.bullet_list.append(bullet)
 
 def main():
     window = Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
